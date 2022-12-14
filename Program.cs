b7382c293d29aa9d7cd9c3d7b3a5649415a09099
k s <klaus.stolz@web.de>
2022-12-14 15:21:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbeitenMitDateien {
+    class Program {
+        static void Main ( string [ ] args ) {
+            FileStream fs = File.Open ( @"c:\einTest\datei.txt", FileMode.Open);
+            StreamReader sr = new StreamReader ( fs );
+            //Console.WriteLine ( sr.ReadToEnd() );
+            //fs.Close();
+            
+
+            string line;
+
+            //Console.WriteLine ( line);
+
+            while ((line = sr.ReadLine() )!= null) {
+                Console.WriteLine ( line);
+            }
+            sr.Close ( );
+
+            fs.Close ( );
+
+            // Schreiben
+
+            FileStream fs2 = File.Open ( "datei.txt" , FileMode.OpenOrCreate );
+            BufferedStream bs2 = new BufferedStream ( fs2 );
+
+            StreamWriter sw = new StreamWriter ( bs2 );
+            sw.WriteLine ( "unsere Daten!");
+            sw.Close ( );
+            bs2.Close ( );
+
+
+
+
+            Console.Read ( );
+
+        }
+    }
+}
